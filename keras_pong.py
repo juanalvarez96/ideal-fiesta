945d6388eeff716e902523c6cee44536679fd2d0
Juan Álvarez Fernández del Vallado <Juan@Juans-MacBook-Pro.local>
2020-04-12 20:09:39 +0200

    Working. Not optimal results

--- keras_pong.py
+++ keras_pong.py
@@ -18,131 +18,44 @@ from keras.layers.advanced_activations import PReLU
 from keras.layers.normalization import BatchNormalization
 from keras.layers.core import Activation, Dropout, Flatten
 from keras.layers.convolutional import UpSampling2D, Convolution2D
-
-#Script Parameters
-input_dim = 80 * 80
-gamma = 0.99
-update_frequency = 1
-learning_rate = 0.001
-resume = False
-render = False
+from keras.models import model_from_json # Needed to load out neural network
+import pdb
+import imageio
 
 #Initialize
 env = gym.make("Pong-v0")
 number_of_inputs = env.action_space.n #This is incorrect for Pong (?)
 #number_of_inputs = 1
 observation = env.reset()
-prev_x = None
-xs, dlogps, drs, probs = [],[],[],[]
-running_reward = None
-reward_sum = 0
-episode_number = 0
-train_X = []
-train_y = []
 
 def pong_preprocess_screen(I):
-  I = I[35:195] 
-  I = I[::2,::2,0] 
-  I[I == 144] = 0 
-  I[I == 109] = 0 
-  I[I != 0] = 1 
-  return I.astype(np.float).ravel()
-def discount_rewards(r):
-  discounted_r = np.zeros_like(r)
-  running_add = 0
-  for t in reversed(range(0, r.size)):
-    if r[t] != 0: running_add = 0
-    running_add = running_add * gamma + r[t]
-    print('reward:{}'.format(running_add))
-    discounted_r[t] = running_add
-  return discounted_r
+  obs_t = I[34:194:4, 12:148:2, 1]
+  return obs_t
 
-#Define the main model (WIP)
-def learning_model(input_dim=80*80, model_type=1):
-  model = Sequential()
-  if model_type==0:
-    model.add(Reshape((1,80,80), input_shape=(input_dim,)))
-    model.add(Flatten())
-    model.add(Dense(200, activation = 'relu'))
-    model.add(Dense(number_of_inputs, activation='softmax'))
-    opt = RMSprop(lr=learning_rate)
-  else:
-    model.add(Reshape((1,80,80), input_shape=(input_dim,)))
-    model.add(Convolution2D(32, 9, 9, subsample=(4, 4), border_mode='same', activation='relu', init='he_uniform'))
-    model.add(Flatten())
-    model.add(Dense(16, activation='relu', init='he_uniform'))
-    model.add(Dense(number_of_inputs, activation='softmax'))
-    opt = Adam(lr=learning_rate)
-  model.compile(loss='categorical_crossentropy', optimizer=opt)
-  if resume == True:
-    model.load_weights('pong_model_checkpoint.h5')
-  return model
 
-model = learning_model()
+json_file = open('model1.json', 'r')
+loaded = json_file.read()
+json_file.close()
+model = model_from_json(loaded)
+
+
 
 #Begin training
 while True:
-  if render: 
-    env.render()
+  #if render: 
+  env.render()
   #Preprocess, consider the frame difference as features
+  #pdb.set_trace()
   cur_x = pong_preprocess_screen(observation)
-  x = cur_x - prev_x if prev_x is not None else np.zeros(input_dim)
-  prev_x = cur_x
-  #Predict probabilities from the Keras model
-  aprob = ((model.predict(x.reshape([1,x.shape[0]]), batch_size=1).flatten()))
-  #aprob = aprob/np.sum(aprob)
-  #Sample action
-  #action = np.random.choice(number_of_inputs, 1, p=aprob)
-  #Append features and labels for the episode-batch
-  xs.append(x)
-  probs.append((model.predict(x.reshape([1,x.shape[0]]), batch_size=1).flatten()))
-  aprob = aprob/np.sum(aprob)
-  action = np.random.choice(number_of_inputs, 1, p=aprob)[0]
-  y = np.zeros([number_of_inputs])
-  y[action] = 1
-  #print action
-  dlogps.append(np.array(y).astype('float32') - aprob)
-  observation, reward, done, info = env.step(action)
-  reward_sum += reward
-  drs.append(reward) 
-  if done:
-    episode_number += 1
-    epx = np.vstack(xs)
-    epdlogp = np.vstack(dlogps)
-    epr = np.vstack(drs)
-    discounted_epr = discount_rewards(epr)
-    discounted_epr -= np.mean(discounted_epr)
-    discounted_epr /= np.std(discounted_epr)
-    epdlogp *= discounted_epr
-    #Slowly prepare the training batch
-    train_X.append(xs) 
-    train_y.append(epdlogp)
-    xs,dlogps,drs = [],[],[]
-    #Periodically update the model
-    if episode_number % update_frequency == 0: 
-      y_train = probs + learning_rate * np.squeeze(np.vstack(train_y)) #Hacky WIP
-      #y_train[y_train<0] = 0
-      #y_train[y_train>1] = 1
-      #y_train = y_train / np.sum(np.abs(y_train), axis=1, keepdims=True)
-      print ('Training Snapshot:')
-      print (y_train)
-      model.train_on_batch(np.squeeze(np.vstack(train_X)), y_train)
-      #Clear the batch
-      train_X = []
-      train_y = []
-      probs = []
-      #Save a checkpoint of the model
-      os.remove('pong_model_checkpoint.h5') if os.path.exists('pong_model_checkpoint.h5') else None
-      model.save_weights('pong_model_checkpoint.h5')
-    #Reset the current environment nad print the current results
-    running_reward = reward_sum if running_reward is None else running_reward * 0.99 + reward_sum * 0.01
-    print ('Environment reset imminent. Total Episode Reward: {}. Running Mean: {}'.format(reward_sum, running_reward))
-    reward_sum = 0
-    observation = env.reset()
-    prev_x = None
-  if reward != 0:
-    if reward == -1:
-        result = "Defeat!"
-    else:
-        result="VICTORY!"
-    print('Episode {} Result: {}'.format(episode_number, result))
+  action = model.predict_classes(cur_x)
+  value = int(sum(action)/len(action))
+  dic = {
+    0:0,
+    -1:3,
+    1:2
+  }
+  value_final = dic.get(value)
+  print(value_final)
+  #pdb.set_trace()
+  #print(value)
+  observation, _,_,_ = env.step(value_final)
--- neural_network.py
+++ neural_network.py
@@ -20,7 +20,72 @@ from keras.layers.normalization import BatchNormalization
 from keras.layers.core import Activation, Dropout, Flatten
 from keras.layers.convolutional import UpSampling2D, Convolution2D
 
+
+
 # Load dataset
 # Now we will read the dataset
 df=pd.read_csv('X.txt', sep = '.', header = None)
-print(df.shape)
+
+# Get X and y vectors
+X = df.iloc[:, 0:68]
+y = df[68].map({2:1, 3:-1, 0:0}) # Careful: This is a pandas series
+
+
+# Define keras models
+def model1():
+    model = Sequential()
+    model.add(Dense(110, input_dim = 68, activation="relu"))
+    model.add(Dense(200, input_dim = 110, activation="relu"))
+    model.add(Dense(30, input_dim = 200, activation="relu"))
+    model.add(Dense(8, input_dim = 30, activation="relu"))
+    model.add(Dense(1, input_dim = 8, activation="sigmoid"))
+    return model
+
+def model2():
+    model = Sequential()
+    model.add(Dense(90, input_dim = 68, activation="relu"))
+    model.add(Dense(150, input_dim = 90, activation="relu"))
+    model.add(Dense(200, input_dim = 150, activation="relu"))
+    model.add(Dense(150, input_dim = 200, activation="relu"))
+    model.add(Dense(90, input_dim = 150, activation="relu"))
+    model.add(Dense(30, input_dim = 90, activation="relu"))
+    model.add(Dense(8, input_dim = 30, activation="relu"))
+    model.add(Dense(1, input_dim = 8, activation="sigmoid"))
+    return model
+
+
+def model3():
+    model = Sequential()
+    model.add(Dense(159, input_dim = 68, activation="relu"))
+    model.add(Dense(100, input_dim = 159, activation="relu"))
+    model.add(Dense(1, input_dim = 100, activation="sigmoid"))    
+    return model
+
+model1 = model1()
+model2 = model2()
+model3 = model3()
+
+models = [model1,model2, model3]
+i = 0
+
+for model in models:
+    # Compile keras models
+    model.compile(loss="binary_crossentropy", optimizer='adam', metrics = ['accuracy'])
+
+    # Fit keras model
+    model.fit(X,y,epochs=7, batch_size=12)
+
+    # Evaluate model
+    _, accuracy=model.evaluate(X,y)
+    print('Accuracy: %.2f' % (accuracy*100))
+
+    # serialize model to JSON
+    model_json = model.to_json()
+    with open("model{}.json".format(i), "w") as json_file:
+        json_file.write(model_json)
+    # serialize weights to HDF5
+    model.save_weights("model.h5")
+    print("Saved model to disk")
+    i = i+1
+
+    
